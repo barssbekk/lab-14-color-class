81d0e0946f475cdfda3fd8f4564f5865678599cf
Barsbek Azamatov <bars.moderside@gmail.com>
2026-03-01 15:43:38 -0800

    Add Color class

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 
 using namespace std;
 
+class Color {
+private:
+    int m_red{};
+    int m_green{};
+    int m_blue{};
+public:
+    // Getter
+    int getRed() const { return m_red; }
+    int getGreen() const { return m_green; };
+    int getBlue() const { return m_blue; };
+};
 int main() {
+    Color myColor{};
+    cout << myColor.getBlue();
     return 0;
 }
